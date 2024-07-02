d3e0ad1ea39e0b58d1fa8f2c151fa7e99a2cc1ae
Badru Siddique <123924787+badrusiddique@users.noreply.github.com>
2024-07-02 17:52:45 +0000

    linked list Q

---
+++ linked-list/reverse-link-list.js
@@ -0,0 +1,45 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (head === null) return head;
+
+    let prev = null;
+    let curr = head;
+
+    while (curr !== null) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    return prev
+};
