b286067c672901a6f4fb6266bb69be35e14407c8
Badru Siddique <123924787+badrusiddique@users.noreply.github.com>
2024-07-06 09:04:40 +0000

    compare tree

---
+++ search/next-greater-letter.js
@@ -0,0 +1,45 @@
+// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+// Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ 
+
+// Example 1:
+
+// Input: letters = ["c","f","j"], target = "a"
+// Output: "c"
+// Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+// Example 2:
+
+// Input: letters = ["c","f","j"], target = "c"
+// Output: "f"
+// Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+// Example 3:
+
+// Input: letters = ["x","x","y","y"], target = "z"
+// Output: "x"
+// Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+ 
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    const targetCode = target.charCodeAt(0);
+
+    for(let l of letters) {
+        if(l.charCodeAt(0) > targetCode) {
+            return l;
+        }
+    }
+
+    // for(let l of letters) {
+    //     if(l > target) {
+    //         return l;
+    //     }
+    // }
+
+    return letters[0];
+};
